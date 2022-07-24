7c3bc7490142f594779d19ed4b1ad9c3898663b0
Shaun Hamilton <shauhami020@gmail.com>
2022-07-24 17:56:56 +0100

    add: vga write

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@
 #![no_main] // disable all Rust-level entry points
 use core::panic::PanicInfo;
 
+static SHAUN_OS: &[u8] = b"ShaunOS";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in SHAUN_OS.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
